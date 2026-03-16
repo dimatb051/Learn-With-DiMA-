225fbdddbc891e6f67c0f5aa1e60b19585397f88
dimatb051 <dimatb051@gmail.com>
2026-03-16 06:21:08 +0300

    Create SumTwoNumbersFixed.java

---
+++ basics/SumTwoNumbersFixed.java
@@ -0,0 +1,13 @@
+// This program calculates the sum of two predefined numbers, and this line called comment..
+
+public class SumTwoNumbersFixed {
+
+    public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 20;
+       int sum = num1 + num2;
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+      // or  System.out.println("Result: " + sum); more easier!
+    }
+}
