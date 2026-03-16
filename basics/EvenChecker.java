1a6543dddc659614d6242f6d351c5e92090f956a
dimatb051 <dimatb051@gmail.com>
2026-03-16 06:11:28 +0300

    Update EvenChecker.java

--- basics/EvenChecker.java
+++ basics/EvenChecker.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-/*
-Check if a number is even or odd */
+// Checking if a number is even or odd by using if statement
 public class EvenChecker {
 public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
       System.out.print("Enter a number: ");
 int num = scanner.nextInt();
-      if (num % 2 == 0) {
+      if (num % 2 == 0) { 
+// If the remainder is zero, it's even
             System.out.println("Number is even"); 
       }
      else {
